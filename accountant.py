7e567db113cee2f2fac77decd977aa5970327d29
Oskar <ookroj95@gmail.com>
2021-04-26 09:54:30 +0200

    expansion works for instructions on the input

--- accountant.py
+++ accountant.py
@@ -1,13 +1,39 @@
 import sys
-print(sys.argv)
-defined_commands = ('saldo')
 command = sys.argv[1:][0]
 #value_input = sys.argv[1:][1]
+#setting allowed commands on input
+allowed_commands = ('saldo', 'sprzedaz', 'zakup', 'konto', 'magazyn', 'przeglad')
+if command in allowed_commands:
+    print(sys.argv[1])  
+    #Instruction for command saldo
+    if sys.argv[1] == ('saldo'):
+        sys.argv[2] = int(sys.argv[2])
+        #setting new names for input's
+        value = sys.argv[2]
+        commentary = sys.argv[3]
+        #Safetycheckprint
+        #print(value, commentary)
+        
+        #Instruction for command sprzedaz
+    elif sys.argv[1] == ('sprzedaz'):    
+            
+            sys.argv[3] = int(sys.argv[3])
+            sys.argv[4] = int(sys.argv[4])
+        #Instruction for command zakup
+    elif sys.argv[1] == ('zakup'):
+            sys.argv[3] = int(sys.argv[3])
+            sys.argv[4] = int(sys.argv[4])
+    else:
+        print("Nie rozpoznano komendy")
 
-if command == 'saldo':
-    print("dziala")
 
 
+
+
+
+
+
+'''
 elif command == 'sprzedaz': 
         print("nie dziala")
 
@@ -22,7 +48,7 @@ elif command == 'przeglad':
         print("dziala przeglad")
 else: 
     print("niepopoprawna komenda")
-'''
+''''''
 lista = sys.argv 
 print(lista[1:]
     if lista[2] == "saldo":
