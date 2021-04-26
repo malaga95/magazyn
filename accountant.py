cca830cbf00e39a65e7c67ee9e602fb6b4ea9db0
Oskar <ookroj95@gmail.com>
2021-04-26 17:31:14 +0200

    I have to switch on input method instead of sys library

--- accountant.py
+++ accountant.py
@@ -2,30 +2,52 @@ import sys
 command = sys.argv[1:][0]
 #value_input = sys.argv[1:][1]
 #setting allowed commands on input
-allowed_commands = ('saldo', 'sprzedaz', 'zakup', 'konto', 'magazyn', 'przeglad')
-if command in allowed_commands:
-    print(sys.argv[1])  
-    #Instruction for command saldo
-    if sys.argv[1] == ('saldo'):
-        sys.argv[2] = int(sys.argv[2])
-        #setting new names for input's
-        value = sys.argv[2]
-        commentary = sys.argv[3]
-        #Safetycheckprint
-        #print(value, commentary)
-        
-        #Instruction for command sprzedaz
-    elif sys.argv[1] == ('sprzedaz'):    
-            
-            sys.argv[3] = int(sys.argv[3])
-            sys.argv[4] = int(sys.argv[4])
-        #Instruction for command zakup
-    elif sys.argv[1] == ('zakup'):
-            sys.argv[3] = int(sys.argv[3])
-            sys.argv[4] = int(sys.argv[4])
-    else:
-        print("Nie rozpoznano komendy")
+allowed_commands = ('saldo', 'sprzedaz', 'zakup', 'konto', 'magazyn', 'przeglad', 'end')
+while True:
 
+        if command in allowed_commands:
+                
+                #Instruction for command saldo
+                if sys.argv[1] == ('saldo'):
+                        sys.argv[2] = int(sys.argv[2])
+                        #setting new names for input's
+                        value = sys.argv[2]
+                        commentary = sys.argv[3]
+                        saldo = {}
+
+                        #Safetycheckprint
+                        #print(value, commentary)
+                        
+                        #Instruction for command sprzedaz
+                elif sys.argv[1] == ('sprzedaz'):    
+                        #converting variables to proper type.
+                        sys.argv[3] = int(sys.argv[3])
+                        sys.argv[4] = int(sys.argv[4])
+                        #Adding names of values
+                        ProductID = sys.argv[2]
+                        product_price = sys.argv[3]
+                        sale_count = sys.argv[4]
+                        sale = {}
+
+                        #Instruction for command zakup
+                elif sys.argv[1] == ('zakup'):
+                        #converting variables to proper type.
+                        sys.argv[3] = int(sys.argv[3])
+                        sys.argv[4] = int(sys.argv[4])
+                        #Adding names of values
+                        ProductID = sys.argv[2]
+                        product_price = sys.argv[3]
+                        buy_count = sys.argv[4]
+                        buy = {}
+        elif command == 'end':
+                break
+        else:
+                continue
+        
+        
+'''else: 
+print("podano niepoprawna komende")
+continue'''
 
 
 
@@ -34,23 +56,23 @@ if command in allowed_commands:
 
 
 '''
-elif command == 'sprzedaz': 
-        print("nie dziala")
+                elif command == 'sprzedaz': 
+                        print("nie dziala")
 
 
-elif command == 'zakup':
-        print("dziala zakup")
-elif command == 'konto':
-        print("dziala konto")
-elif command == 'magazyn':
-        print("magazyn dziala")
-elif command == 'przeglad':
-        print("dziala przeglad")
-else: 
-    print("niepopoprawna komenda")
-''''''
-lista = sys.argv 
-print(lista[1:]
-    if lista[2] == "saldo":
-    print("dziala")
-  '''
+                elif command == 'zakup':
+                        print("dziala zakup")
+                elif command == 'konto':
+                        print("dziala konto")
+                elif command == 'magazyn':
+                        print("magazyn dziala")
+                elif command == 'przeglad':
+                        print("dziala przeglad")
+                else: 
+                print("niepopoprawna komenda")
+                ''''''
+                lista = sys.argv 
+                print(lista[1:]
+                if lista[2] == "saldo":
+                print("dziala")
+'''
